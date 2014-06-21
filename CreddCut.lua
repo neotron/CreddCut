999907fb2d473451fc7b8674f6a0b893f9c4454a
David Hedbor <neotron@gmail.com>
2014-06-20 19:27:42 -0700

    Initial version

---
+++ CreddCut.lua
@@ -0,0 +1,28 @@
+require "CREDDExchangeLib"
+
+local CreddCut = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:NewAddon("CREDDCut", false, { "MarketplaceCREDD" }, "Gemini:Hook-1.0" )
+
+function CreddCut:OnEnable()
+   local credd = Apollo.GetAddon("MarketplaceCREDD")
+   self:PostHook(credd, "OnCREDDExchangeInfoResults")
+   self:PostHook(credd, "OnHeaderTabCheck", "UpdatePrice")
+end
+
+function CreddCut:OnCREDDExchangeInfoResults(mp, stats, orders)
+   if stats then
+      if stats.arBuyOrderPrices  then
+	 self.buyCost = stats.arBuyOrderPrices[1].monPrice:GetAmount() + 1
+      end
+      if stats.arSellOrderPrices  then
+	 self.sellCost = stats.arSellOrderPrices[1].monPrice:GetAmount() - 1
+      end
+      self:UpdatePrice(mp)
+   end
+end
+
+function CreddCut:UpdatePrice(mp)
+   local bBuyTabChecked = mp.tWindowMap["HeaderBuyBtn"]:IsChecked()
+   mp.tWindowMap["ActLaterPrice"]:SetAmount( (bBuyTabChecked and self.buyCost or self.sellCost) or 0)
+   mp.tWindowMap["ActLaterPrice"]:SetTextColor(bBuyTabChecked and ApolloColor.new("ffc2e57f") or ApolloColor.new("UI_TextHoloBody"))
+   mp:RefreshBoundCredd()
+end
