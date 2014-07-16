5c7cb6bebc98a31d3dcf9ed5702c474462572100
David Hedbor <neotron@gmail.com>
2014-07-15 23:45:54 -0700

    API version bump.

--- CreddCut.lua
+++ CreddCut.lua
@@ -21,6 +21,9 @@ function CreddCut:OnCREDDExchangeInfoResults(mp, stats, orders)
 end
 
 function CreddCut:UpdatePrice(mp)
+   if not mp or not mp.tWindowMap or not mp.tWindowMap["HeaderBuyBtn"] or not mp.tWindowMap["ActLaterPrice"] then
+      return
+   end
    local bBuyTabChecked = mp.tWindowMap["HeaderBuyBtn"]:IsChecked()
    mp.tWindowMap["ActLaterPrice"]:SetAmount( (bBuyTabChecked and self.buyCost or self.sellCost) or 0)
    mp.tWindowMap["ActLaterPrice"]:SetTextColor(bBuyTabChecked and ApolloColor.new("ffc2e57f") or ApolloColor.new("UI_TextHoloBody"))
